457fe344a0f0a59bab7e7c468ff8ea9a2eb3ef0e
Hardikgithub2020 <hardik.unity@gmail.com>
2020-07-31 00:15:51 -0400

    Create Pseudocode

--- react_box_color/src/App.js
+++ react_box_color/src/App.js
@@ -1,3 +1,16 @@
+// Component has two properties, Count and color 
+// Both value are mutable 
+// We create class component
+// also we need to compbach on initial state , so we saved initial state value 
+// create get function to reassigned initial value 
+//color need 6 digit value, so we create for loop which give a different string every time 
+// We create a siv, and set attribute type: 'button' to make it clickble
+// we call a function which coungt incremental by q on each click and alos assign a new color value 
+//Create one button for reset, onclick call function which assign default value to State
+
+
+
+
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
@@ -43,6 +56,7 @@ class App extends React.Component {
       <div className='App'>
       <div className='App'type="button" style={styleValue} onClick={this.clickUpdate}>
        <h1 className='count'> {this.state.count}</h1>
+       <h6 className='count'>Click on me, I am changing color and also count how many times you click</h6>
         
       </div>
       <button type="button" onClick={this.resetBuilder}>
