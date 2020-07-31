fca9aa165e4a553b91f43926a3855e3350c25f75
Hardikgithub2020 <hardik.unity@gmail.com>
2020-07-30 23:49:52 -0400

    Completecode

--- react_box_color/src/App.js
+++ react_box_color/src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      color: "#32a84e",
+    };
+  }
+  get initialState() {
+    return {
+      count: 0,
+      color: "#32a84e",
+    };
+  }
+
+  clickUpdate = () => {
+    // letter is a 16 digit string and everytime function create new string
+    let letters = "0123456789ABCDEF";
+    let rainbow = "#";
+    for (let i = 0; i < 6; i++) {
+      rainbow += letters[Math.floor(Math.random() * 16)];
+    }
+    this.setState({
+      count: this.state.count + 1,
+      color: rainbow,
+    });
+  }
+  resetBuilder= () => {
+    this.setState(this.initialState);
+  }
+
+  render() {
+    let styleValue = {
+      backgroundColor: this.state.color,
+      width: "50%",
+      height: 200,
+    };
+    return (
+      <div className='App'>
+      <div className='App'type="button" style={styleValue} onClick={this.clickUpdate}>
+       <h1 className='count'> {this.state.count}</h1>
+        
+      </div>
+      <button type="button" onClick={this.resetBuilder}>
+      Reset
+    </button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
